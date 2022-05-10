97f73796343e76315dbe0a4a1b5d8e39ac93be92
GEAMER007 <pavel15062007mail.ru@yandex.ru>
2022-05-10 10:57:46 +0300

    a

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 var input=process.argv[2]
 var output=process.argv[3]
-require('./compiler')(process.argv[3])
+if(!output&&!!input){
+    const path =require("path")
+    output= `${path.dirname(input)}/${path.basename(input,'.hlnb')}.njsbcs`
+}
+if(!input){console.error("no input provided:(");process.exit(1)}
+require('./compiler')(input,output)
